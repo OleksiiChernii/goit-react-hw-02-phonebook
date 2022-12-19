65c732b7aeac0e56f110bdd15b28ce74fb0b29db
OleksiiChernii <oleksii.chernii@gmail.com>
2022-12-19 20:09:47 +0200

    task complited

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import {Phonebook, Contacts} from './Phonebook';
+
 export const App = () => {
+  const [state, setState] = useState({
+    contacts: [],
+    name: '',
+    number: '',
+    filter: ''
+  });
+
+  const handler = e => {
+    e.preventDefault();
+    const [name, number] = e.target;
+
+    const {contacts} = state;
+    setState({
+      contacts: [...contacts, { name: name.value, number: number.value ,id: nanoid() }],
+      name: name.value,
+      number: number.value
+    });
+    name.value = '';
+    number.value = '';
+  };
+
+  const filterHandler = e => {
+    const filter = e.target.value;
+    setState({...state, filter});
+  }
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  <>
+    <Phonebook handler={handler} />
+    <Contacts state={state} filterHandler={filterHandler}/>
+  </>);
 };
---
+++ src/components/Phonebook.js
@@ -0,0 +1,76 @@
+import PropTypes from 'prop-types';
+
+export function Phonebook({ handler }) {
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <form onSubmit={handler}>
+        <label>
+          Name
+          <br />
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Number
+          <br />
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Add contact</button>
+      </form>
+    </>
+  );
+}
+
+Phonebook.propTypes = {
+  handler: PropTypes.func.isRequired,
+};
+
+export function Contacts({ state, filterHandler }) {
+  const { contacts, filter } = state;
+  return (
+    <>
+      <h1>Contacts</h1>
+      <p>Find contacts by name</p>
+      <input type="text" name="filter" onInput={filterHandler} />
+      <ul>
+        {contacts
+          .filter(
+            ({ name }) =>
+              !filter || name.toLowerCase().startsWith(filter.toLowerCase())
+          )
+          .map(({ id, name, number }) => (
+            <li key={id}>
+              {name}: {number}
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+}
+
+Contacts.propTypes = {
+  state: PropTypes.shape({
+    contacts: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+      }).isRequired
+    ).isRequired,
+  }).isRequired,
+  filterHandler: PropTypes.func.isRequired,
+};
